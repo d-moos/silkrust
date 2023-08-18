5e2887999ef754686a6ab3172712f00a3d8e019e
Daniel Moos <danile@localhost>
2023-08-18 12:22:36 +0200

    rename: type -> kind

--- src/net/message/id.rs
+++ src/net/message/id.rs
@@ -6,12 +6,12 @@ const OPERATION_SIZE: u16 = 12;
 const OPERATION_OFFSET: u16 = 0;
 const OPERATION_MASK: u16 = ((1 << OPERATION_SIZE) - 1) << OPERATION_OFFSET;
 
-const TYPE_SIZE: u16 = 2;
-const TYPE_OFFSET: u16 = OPERATION_OFFSET + OPERATION_SIZE;
-const TYPE_MASK: u16 = ((1 << TYPE_SIZE) - 1) << TYPE_OFFSET;
+const KIND_SIZE: u16 = 2;
+const KIND_OFFSET: u16 = OPERATION_OFFSET + OPERATION_SIZE;
+const KIND_MASK: u16 = ((1 << KIND_SIZE) - 1) << KIND_OFFSET;
 
 const DIRECTION_SIZE: u16 = 2;
-const DIRECTION_OFFSET: u16 = TYPE_OFFSET + TYPE_SIZE;
+const DIRECTION_OFFSET: u16 = KIND_OFFSET + KIND_SIZE;
 const DIRECTION_MASK: u16 = ((1 << DIRECTION_SIZE) - 1) << DIRECTION_OFFSET;
 
 pub type MessageOperation = u16;
@@ -59,7 +59,7 @@ impl From<u16> for MessageId {
     fn from(value: u16) -> Self {
         MessageId {
             direction: MessageDirection::from(((value & DIRECTION_MASK) >> DIRECTION_OFFSET) as u8),
-            category: MessageKind::from(((value & TYPE_MASK) >> TYPE_OFFSET) as u8),
+            category: MessageKind::from(((value & KIND_MASK) >> KIND_OFFSET) as u8),
             operation: ((value & OPERATION_MASK) >> OPERATION_OFFSET),
         }
     }
@@ -70,8 +70,8 @@ impl Into<u16> for MessageId {
         let mut value: u16 = 0;
         value = (value & !DIRECTION_MASK)
             | (((self.direction as u16) << DIRECTION_OFFSET) & DIRECTION_MASK);
-        value = (value & !TYPE_MASK)
-            | (((Into::<u8>::into(self.category) as u16) << TYPE_OFFSET) & TYPE_MASK);
+        value = (value & !KIND_MASK)
+            | (((Into::<u8>::into(self.category) as u16) << KIND_OFFSET) & KIND_MASK);
         value =
             (value & !OPERATION_MASK) | (((self.operation) << OPERATION_OFFSET) & OPERATION_MASK);
         value
